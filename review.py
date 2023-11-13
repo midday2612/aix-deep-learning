d6805802b80e0165cc222936957570834d55f9e6
midday2612 <midday2612@gmail.com>
2023-11-13 18:35:39 +0900

    data cleansing

--- review.py
+++ review.py
@@ -22,3 +22,49 @@ train_data[:5] # 상위 5개 출력
 print('테스트용 리뷰 개수 :',len(test_data)) # 테스트용 리뷰 개수 출력
 
 test_data[:5]
+
+# document 열과 label 열의 중복을 제외한 값의 개수
+train_data['document'].nunique(), train_data['label'].nunique()
+
+# document 열의 중복 제거
+train_data.drop_duplicates(subset=['document'], inplace=True)
+
+print('총 샘플의 수 :',len(train_data))
+
+train_data['label'].value_counts().plot(kind = 'bar')
+
+print(train_data.groupby('label').size().reset_index(name = 'count'))
+print(train_data.isnull().values.any())
+print(train_data.isnull().sum())
+
+train_data.loc[train_data.document.isnull()]
+train_data = train_data.dropna(how = 'any') # Null 값이 존재하는 행 제거
+
+print(train_data.isnull().values.any()) # Null 값이 존재하는지 확인
+print(len(train_data))
+
+#알파벳과 공백을 제외하고 모두 제거
+eng_text = 'do!!! you expect... people~ to~ read~ the FAQ, etc. and actually accept hard~! atheism?@@'
+print(re.sub(r'[^a-zA-Z ]', '', eng_text))
+
+# 한글과 공백을 제외하고 모두 제거
+train_data['document'] = train_data['document'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")
+train_data[:5]
+train_data['document'] = train_data['document'].str.replace('^ +', "") # white space 데이터를 empty value로 변경
+train_data['document'].replace('', np.nan, inplace=True)
+
+print(train_data.isnull().sum())
+
+train_data.loc[train_data.document.isnull()][:5]
+train_data = train_data.dropna(how = 'any')
+
+print(len(train_data))
+
+
+test_data.drop_duplicates(subset = ['document'], inplace=True) # document 열에서 중복인 내용이 있다면 중복 제거
+test_data['document'] = test_data['document'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","") # 정규 표현식 수행
+test_data['document'] = test_data['document'].str.replace('^ +', "") # 공백은 empty 값으로 변경
+test_data['document'].replace('', np.nan, inplace=True) # 공백은 Null 값으로 변경
+test_data = test_data.dropna(how='any') # Null 값 제거
+
+print('전처리 후 테스트용 샘플의 개수 :',len(test_data))
