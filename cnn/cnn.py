b6e062e9cb4ad771fb401a5cb1a617a24868c3b5
midday2612 <midday2612@gmail.com>
2023-11-20 02:07:31 +0900

    cnn

---
+++ cnn/cnn.py
@@ -0,0 +1,100 @@
+from tensorflow.keras.layers import Embedding, Dense, LSTM
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import load_model
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+import pickle
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import urllib.request
+from konlpy.tag import Okt
+from tqdm import tqdm
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Embedding, Dropout, Conv1D, GlobalMaxPooling1D, Dense, Input, Flatten, Concatenate
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.models import load_model
+
+# Load X_train from the text file
+X_train = np.loadtxt('../data_prep/prep_X_train.txt', delimiter=' ', dtype=int)
+
+# Load X_test from the text file
+X_test = np.loadtxt('../data_prep/prep_X_test.txt', delimiter=' ', dtype=int)
+
+# Load y_train from the text file
+y_train = np.loadtxt('../data_prep/prep_y_train.txt', dtype=int)
+
+# Load y_test from the text file
+y_test = np.loadtxt('../data_prep/prep_y_test.txt', dtype=int)
+
+# 파일에서 저장된 정보 불러오기
+with open('../data_prep/prep_info.txt', 'r', encoding='utf-8') as file:
+    loaded_info = file.readlines()
+
+# 불러온 정보 처리
+max_len = int(loaded_info[0].split(': ')[1].strip())
+vocab_size = int(loaded_info[1].split(': ')[1].strip())
+stopwords_str = loaded_info[2].split(': ')[1].strip()
+
+# 문자열로 저장된 리스트를 eval 함수를 사용하여 리스트로 변환
+stopwords = eval(stopwords_str)
+
+with open('../data_prep/tokenizer.pickle', 'rb') as file:
+    tokenizer = pickle.load(file)
+
+embedding_dim = 128
+dropout_ratio = (0.5, 0.8)
+num_filters = 128
+hidden_units = 128
+
+model_input = Input(shape = (max_len,))
+z = Embedding(vocab_size, embedding_dim, input_length = max_len, name="embedding")(model_input)
+z = Dropout(dropout_ratio[0])(z)
+
+conv_blocks = []
+
+for sz in [3, 4, 5]:
+    conv = Conv1D(filters = num_filters,
+                         kernel_size = sz,
+                         padding = "valid",
+                         activation = "relu",
+                         strides = 1)(z)
+    conv = GlobalMaxPooling1D()(conv)
+    conv_blocks.append(conv)
+
+z = Concatenate()(conv_blocks) if len(conv_blocks) > 1 else conv_blocks[0]
+z = Dropout(dropout_ratio[1])(z)
+z = Dense(hidden_units, activation="relu")(z)
+model_output = Dense(1, activation="sigmoid")(z)
+
+model = Model(model_input, model_output)
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["acc"])
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
+mc = ModelCheckpoint('CNN_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+
+model.fit(X_train, y_train, batch_size=64, epochs=10, validation_split=0.2, verbose=2, callbacks=[es, mc])
+
+loaded_model = load_model('CNN_model.h5')
+print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
+
+def sentiment_predict(new_sentence):
+  okt = Okt()
+  new_sentence = re.sub(r'[^ㄱ-ㅎㅏ-ㅣ가-힣 ]','', new_sentence)
+  new_sentence = okt.morphs(new_sentence, stem=True) # 토큰화
+  new_sentence = [word for word in new_sentence if not word in stopwords] # 불용어 제거
+  encoded = tokenizer.texts_to_sequences([new_sentence]) # 정수 인코딩
+  pad_new = pad_sequences(encoded, maxlen = max_len) # 패딩
+  score = float(loaded_model.predict(pad_new)) # 예측
+  if(score > 0.5):
+    print("{:.2f}% 확률로 긍정 리뷰입니다.\n".format(score * 100))
+  else:
+    print("{:.2f}% 확률로 부정 리뷰입니다.\n".format((1 - score) * 100))
+
+sentiment_predict('이 영화 개꿀잼 ㅋㅋㅋ')
+sentiment_predict('이 영화 핵노잼 ㅠㅠ')
+sentiment_predict('이딴게 영화냐 ㅉㅉ')
